6d98f54021d7e109955cdadf5050c278c8be6f4f
been <leebin0603@naver.com>
2025-03-27 21:19:51 +0900

    [chore] CGColor 추가

---
+++ Animori/DesignSystem/Extension/CGColor+.swift
@@ -0,0 +1,16 @@
+//
+//  CGColor+.swift
+//  Animori
+//
+//  Created by 이빈 on 3/27/25.
+//
+
+import UIKit
+
+extension CGColor {
+    
+    static func am(_ amColor: AMColor) -> CGColor {
+        return amColor.color.cgColor
+    }
+    
+}
