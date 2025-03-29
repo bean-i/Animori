f7851ae126a7d93fd8969180b4ddebc4706554e4
been <leebin0603@naver.com>
2025-03-29 20:20:23 +0900

    [feat] #7 BaseFSPagerViewCell 설정

--- Animori/Base/BaseFSPagerViewCell.swift
+++ Animori/Base/BaseFSPagerViewCell.swift
@@ -5,4 +5,27 @@
 //  Created by 이빈 on 3/28/25.
 //
 
-import Foundation
+import UIKit
+import FSPagerView
+
+class BaseFSPagerViewCell: FSPagerViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+    
+    func configureView() { }
+    
+}
