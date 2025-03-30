6fad8c73657ab373ce2df5ff41d8f73648c5257d
been <leebin0603@naver.com>
2025-03-30 20:37:26 +0900

    [feat] #7 Recommend 헤더 열거형 구현

---
+++ Animori/Presentation/Explore/Model/RecommendOption.swift
@@ -0,0 +1,25 @@
+//
+//  RecommendOption.swift
+//  Animori
+//
+//  Created by 이빈 on 3/30/25.
+//
+
+import Foundation
+
+enum RecommendOption: String, CaseIterable {
+    case season
+    case complete
+    case short
+    
+    var displayName: String {
+        switch self {
+        case .season:
+            return LocalizedStrings.RecommendOption.season
+        case .complete:
+            return LocalizedStrings.RecommendOption.complete
+        case .short:
+            return LocalizedStrings.RecommendOption.short
+        }
+    }
+}
