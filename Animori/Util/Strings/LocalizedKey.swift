968ca3652d4d7ce67e2a9fc1aa1a271bee47eaa2
been <leebin0603@naver.com>
2025-03-30 20:14:57 +0900

    [chore] #7 다국어 설정

--- Animori/Util/Strings/LocalizedKey.swift
+++ Animori/Util/Strings/LocalizedKey.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+enum LocalizedKey {
+    
+    enum Sort {
+        static let popular = "sort_by_popularity"
+        static let liked = "sort_by_likes"
+        static let airing = "currently_airing"
+        static let upcoming = "upcoming_broadcast"
+    }
+    
+}
--- Animori/Util/Strings/LocalizedStrings.swift
+++ Animori/Util/Strings/LocalizedStrings.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+enum LocalizedStrings {
+    
+    enum Sort {
+        static let popular = NSLocalizedString(LocalizedKey.Sort.popular, comment: "")
+        static let liked = NSLocalizedString(LocalizedKey.Sort.liked, comment: "")
+        static let airing = NSLocalizedString(LocalizedKey.Sort.airing, comment: "")
+        static let upcoming = NSLocalizedString(LocalizedKey.Sort.upcoming, comment: "")
+    }
+    
+}
