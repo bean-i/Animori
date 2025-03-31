c5906ea928917cbad82b387e7bcb89b9f36f3e88
been <leebin0603@naver.com>
2025-04-01 02:05:38 +0900

    [fix] #11 EndPoint 쿼리 파라미터 인코딩 로직 변경

---
+++ Animori/Core/Network/Anime/AnimeEndPoint.swift
@@ -0,0 +1,58 @@
+//
+//  AnimeEndPoint.swift
+//  Animori
+//
+//  Created by 이빈 on 3/31/25.
+//
+
+import Foundation
+
+enum AnimeEndPoint: EndPoint {
+    
+    case topAnime // getTopAnime
+    case seasonNow // getSeasonNow
+    case completeAnime // getAnimeSearch
+    case movieAnime // getAnimeSearch
+    
+    var baseURL: String? { return Bundle.main.baseURL }
+    var path: String {
+        switch self {
+        case .topAnime: return "/top/anime"
+        case .seasonNow: return "/seasons/now"
+        case .completeAnime, .movieAnime: return "/anime"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .topAnime: return .get
+        case .seasonNow: return .get
+        case .completeAnime: return .get
+        case .movieAnime: return .get
+        }
+    }
+    
+    var decoder: JSONDecoder { return JSONDecoder() }
+    
+    var encoder: JSONEncoder { return JSONEncoder() }
+    
+    var parameters: Encodable {
+        switch self {
+        case .topAnime:
+            return AnimeRequestDTO.topAnime.queryParameters
+        case .seasonNow:
+            return AnimeRequestDTO.seasonNow.queryParameters
+        case .completeAnime:
+            return AnimeRequestDTO.completeAnime.queryParameters
+//            return CompleteAnimeRequest()
+        case .movieAnime:
+            return AnimeRequestDTO.movieAnime.queryParameters
+//            return MovieAnimeRequest()
+        }
+    }
+    
+    func error(_ statusCode: Int?, data: Data) -> any Error {
+        return URLError(.unknown) // 임시
+    }
+    
+}
---
+++ Animori/Model/DTOs/AnimeRequestDTO .swift
@@ -0,0 +1,8 @@
+//
+//  AnimeRequestDTO .swift
+//  Animori
+//
+//  Created by 이빈 on 3/31/25.
+//
+
+import Foundation
