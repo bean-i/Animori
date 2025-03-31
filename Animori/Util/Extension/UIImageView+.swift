ad19a6a9015ae0579d0e4c8c00fa153ca7184937
been <leebin0603@naver.com>
2025-03-31 18:04:20 +0900

    [chore] #9 주석제거

--- Animori/Util/Extension/UIImageView+.swift
+++ Animori/Util/Extension/UIImageView+.swift
@@ -21,15 +21,6 @@ extension UIImageView {
                 self.image = UIImage(systemName: "arrow.down.app.dashed.trianglebadge.exclamationmark")
             }
         }
-        
-//        ImageClient.shared.requestImage(with: imageUrl) { response in
-//            switch response {
-//            case .success(let data):
-//                self.image = UIImage(data: data)
-//            case .failure(_):
-//                self.image = UIImage(systemName: "arrow.down.app.dashed.trianglebadge.exclamationmark")
-//            }
-//        }
     }
     
 }
