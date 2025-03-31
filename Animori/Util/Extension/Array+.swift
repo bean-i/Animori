97735ed3a955a7fa9d5760c09ca43bb7fd84694e
been <leebin0603@naver.com>
2025-04-01 02:11:33 +0900

    [feat] #11 Array 확장(중복 ID 제거 로직 구현

--- Animori/Util/Extension/Array+.swift
+++ Animori/Util/Extension/Array+.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+extension Array where Element: AnimeProtocol {
+    func removeDuplicates() -> [Element] {
+        var seenIds = Set<Int>()
+        return self.filter { anime in
+            if seenIds.contains(anime.id) {
+                return false
+            } else {
+                seenIds.insert(anime.id)
+                return true
+            }
+        }
+    }
+}
