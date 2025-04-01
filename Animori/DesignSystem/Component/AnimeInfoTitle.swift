f8cb5abb79dfdd29057a2732cb2592ae19d55c08
been <leebin0603@naver.com>
2025-04-02 02:04:35 +0900

    [feat] #13 상세화면 타이틀 컴포넌트 구현

--- Animori/DesignSystem/Component/AnimeInfoTitle.swift
+++ Animori/DesignSystem/Component/AnimeInfoTitle.swift
@@ -1,5 +1,5 @@
 //
-//  AnimeInfoBoxView.swift
+//  AnimeInfoTitle.swift
 //  Animori
 //
 //  Created by 이빈 on 4/1/25.
@@ -8,12 +8,9 @@
 import UIKit
 import SnapKit
 
-final class AnimeInfoBoxView: BaseView {
+final class AnimeInfoTitle: BaseView {
     
-    private let contentStackView = UIStackView()
     private let titleLabel = UILabel()
-    private let detailView = UIView()
-    
     private let title: String
     
     init(title: String) {
@@ -22,45 +19,20 @@ final class AnimeInfoBoxView: BaseView {
     }
     
     override func configureHierarchy() {
-        contentStackView.addArrangedSubviews(titleLabel, detailView)
-        addSubview(contentStackView)
+        addSubview(titleLabel)
     }
     
     override func configureLayout() {
-        contentStackView.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.horizontalEdges.equalToSuperview().inset(20)
+        titleLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
-        
-//        titleLabel.snp.makeConstraints { make in
-//            make.leading.centerY.equalToSuperview()
-//        }
-//        
-//        detailView.snp.makeConstraints { make in
-//            make.leading.equalTo(titleLabel.snp.trailing).offset(8)
-//            make.centerY.equalToSuperview()
-//        }
     }
     
     override func configureView() {
-        contentStackView.backgroundColor = .red
-        contentStackView.axis = .horizontal
-        contentStackView.spacing = 8
-        contentStackView.alignment = .center
-        
-        titleLabel.text = "\(title) |"
+        titleLabel.text = title
         titleLabel.font = .am(.bodyMedium)
         titleLabel.textColor = .am(.base(.white))
         titleLabel.textAlignment = .left
-        
-        detailView.backgroundColor = .blue
-    }
-    
-    func configureCustomView(_ view: UIView) {
-        detailView.addSubview(view)
-        view.snp.makeConstraints { make in
-            make.top.horizontalEdges.equalToSuperview()
-        }
     }
     
 }
