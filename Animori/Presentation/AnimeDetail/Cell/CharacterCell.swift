8a7741ebef8c0c3654a195e8d65fd17f45ae9428
been <leebin0603@naver.com>
2025-04-02 18:38:49 +0900

    [feat] #13 캐릭터 셀 구현

--- Animori/Presentation/AnimeDetail/Cell/CharacterCell.swift
+++ Animori/Presentation/AnimeDetail/Cell/CharacterCell.swift
@@ -5,4 +5,49 @@
 //  Created by 이빈 on 4/2/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class CharacterCell: BaseCollectionViewCell {
+    
+    static let identifier = "CharacterCell"
+    
+    private let imageView = UIImageView()
+    private let nameLabel = UILabel()
+    
+    override func configureHierarchy() {
+        contentView.addSubViews(imageView, nameLabel)
+    }
+    
+    override func configureLayout() {
+        imageView.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview()
+            make.size.equalTo(100)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(8)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    override func configureView() {
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 50
+        imageView.contentMode = .scaleAspectFill
+        imageView.tintColor = .am(.base(.gray))
+        imageView.backgroundColor = .am(.base(.gray))
+        
+        nameLabel.font = .am(.bodyMedium)
+        nameLabel.textColor = .am(.base(.white))
+        nameLabel.textAlignment = .center
+        nameLabel.numberOfLines = 0
+    }
+    
+    func configureData(image: String, name: String) {
+        imageView.setImage(with: image)
+        nameLabel.text = name
+    }
+    
+}
