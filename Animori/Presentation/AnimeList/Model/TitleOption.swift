899857048f869e0392d62f474b3a2d82c16d3ad7
been <leebin0603@naver.com>
2025-04-04 16:38:52 +0900

    [feat] #18 AnimeList 타이틀 옵션 구현

--- Animori/Presentation/AnimeList/Model/TitleOption.swift
+++ Animori/Presentation/AnimeList/Model/TitleOption.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+enum TitleOption {
+    case seasonNow
+    case complete
+    case movie
+    case search(String)
+    
+    var displayName: String {
+        switch self {
+        case .seasonNow: return "이번 시즌 애니메이션"
+        case .complete: return "완결 애니메이션"
+        case .movie: return "극장판 애니메이션"
+        case .search(let searchKeyword): return searchKeyword
+        }
+    }
+}
