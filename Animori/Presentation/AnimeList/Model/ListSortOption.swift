c22a9ff55df6dfee817ec9c52fa2598a58f64202
been <leebin0603@naver.com>
2025-04-04 16:38:15 +0900

    [feat] #18 AnimeList 정렬 옵션 구현

--- Animori/Presentation/AnimeList/Model/ListSortOption.swift
+++ Animori/Presentation/AnimeList/Model/ListSortOption.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+enum ListSortOption: Int, CaseIterable {
+    
+    case popularity
+    case favorites
+    case score
+
+    var displayName: String {
+        switch self {
+        case .popularity: return "인기순"
+        case .favorites: return "좋아요순"
+        case .score: return "별점순"
+        }
+    }
+
+    var apiParameter: String {
+        switch self {
+        case .popularity: return "popularity"
+        case .favorites: return "favorites"
+        case .score: return "score"
+        }
+    }
+}
