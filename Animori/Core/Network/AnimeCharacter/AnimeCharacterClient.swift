504caf9234e2c33312e1187fe8960175a1da8e69
been <leebin0603@naver.com>
2025-04-05 16:28:57 +0900

    [feat] #17 캐릭터 네트워크 구현

--- Animori/Core/Network/AnimeCharacter/AnimeCharacterClient.swift
+++ Animori/Core/Network/AnimeCharacter/AnimeCharacterClient.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import RxSwift
+
+final class AnimeCharacterClient {
+    
+    static let shared = AnimeCharacterClient()
+    private let provider = NetworkProvider<AnimeCharacterEndPoint>()
+    
+    private init() { }
+    
+    func getTopCharacters() -> Single<TopCharacterResponseDTO> {
+        return provider.request(.topCharacter)
+            .catch { error in
+                return Single.just(TopCharacterResponseDTO.empty)
+            }
+    }
+    
+}
