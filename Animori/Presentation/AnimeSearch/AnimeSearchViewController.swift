bb19f2bdd5d619e330f29195045dc1ccdc10ff37
been <leebin0603@naver.com>
2025-04-05 16:34:09 +0900

    [feat] #17 검색화면 장르, 애니메이션, 캐릭터 로직 구현

--- Animori/Presentation/AnimeSearch/AnimeSearchViewController.swift
+++ Animori/Presentation/AnimeSearch/AnimeSearchViewController.swift
@@ -5,4 +5,82 @@
 //  Created by 이빈 on 4/4/25.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+import RxDataSources
+
+final class AnimeSearchViewController: BaseViewController<AnimeSearchView> {
+    
+    private let sectionRelay = BehaviorRelay<[AnimeSearchSection]>(value: [])
+    var disposeBag = DisposeBag()
+    
+    init(reactor: AnimeSearchViewModel) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+    
+    override func configureNavigation() { title = "검색" }
+
+    private var dataSource = RxCollectionViewSectionedReloadDataSource<AnimeSearchSection> { dataSource, collectionView, indexPath, item in
+        switch item {
+        case .recentSearch(let recentKeywords):
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecentSearchCell.identifier, for: indexPath) as! RecentSearchCell
+            cell.configureData(with: recentKeywords)
+            return cell
+            
+        case .genreSearch(let genres):
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GenreSearchCell.identifier, for: indexPath) as! GenreSearchCell
+            cell.configureData(with: genres)
+            return cell
+            
+        case .topAnime(let anime):
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecommendCollectionViewCell.identifier, for: indexPath) as! RecommendCollectionViewCell
+            cell.configureData(with: anime)
+            return cell
+            
+        case .topCharacter(let character):
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterInfoCell.identifier, for: indexPath) as! CharacterInfoCell
+            cell.configureData(with: character)
+            return cell
+        }
+    } configureSupplementaryView: { dataSource, collectionView, kind, indexPath in
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: SectionHeaderView.identifier, for: indexPath) as! SectionHeaderView
+        let section = dataSource.sectionModels[indexPath.section]
+        header.configure(with: section.header)
+        return header
+    }
+}
+
+extension AnimeSearchViewController: View {
+    
+    func bind(reactor: AnimeSearchViewModel) {
+        // 데이터 로드
+        reactor.action.onNext(.loadInfo)
+        
+        reactor.state
+            .map { state -> [AnimeSearchSection] in
+                let keywordSection = AnimeSearchSection(header: "최근 검색어",
+                                                        items: state.recentKeywords.map { .recentSearch($0) })
+                
+                let genreSection = AnimeSearchSection(header: "장르 바로가기",
+                                                      items: state.genres.map { .genreSearch($0) })
+                
+                let topAnimeSection = AnimeSearchSection(header: "Top 애니메이션",
+                                                         items: state.topAnimes.map { .topAnime($0) })
+                
+                let topCharacterSection = AnimeSearchSection(header: "Top 캐릭터",
+                                                             items: state.topCharacters.map { .topCharacter($0) })
+                
+                return [keywordSection, genreSection, topAnimeSection, topCharacterSection]
+            }
+            .bind(to: sectionRelay)
+            .disposed(by: disposeBag)
+        
+        sectionRelay
+            .bind(to: mainView.collectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+    }
+    
+}
--- Animori/Presentation/AnimeSearch/AnimeSearchViewModel.swift
+++ Animori/Presentation/AnimeSearch/AnimeSearchViewModel.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+import RxSwift
+import ReactorKit
+
+final class AnimeSearchViewModel: Reactor {
+    
+    enum Action {
+        case loadInfo // 최근 검색어, 장르, 탑애니메이션, 탑캐릭터 다 로드
+    }
+    
+    enum Mutation {
+        case setRecentKeywords([String])
+        case setGenres([any AnimeGenreProtocol])
+        case setTopAnimes([any AnimeProtocol])
+        case setTopCharacters([any TopCharacterProtocol])
+    }
+    
+    struct State {
+        var recentKeywords: [String] = []
+        var genres: [any AnimeGenreProtocol] = []
+        var topAnimes: [any AnimeProtocol] = []
+        var topCharacters: [any TopCharacterProtocol] = []
+    }
+    
+    let initialState: State
+    
+    var keywords = ["asdf", "482", "sdf", "gjlgk", "fwijorij", "rejhdsfjh", "sdlkjf"]
+    
+    init(initialState: State) {
+        self.initialState = initialState
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .loadInfo:
+            let topAnime = AnimeClient.shared.getTopAnime(query: TopAnimeRequest.basic)
+                .map { $0.data.map { $0.toEntity() }.removeDuplicates() }
+                .map { Mutation.setTopAnimes($0) }
+                .asObservable()
+
+            let topCharacters = AnimeCharacterClient.shared.getTopCharacters()
+                .map { $0.data.map { $0.toEntity() } }
+                .map { Mutation.setTopCharacters($0) }
+                .asObservable()
+            
+            let genres = AnimeGenreClient.shared.getAnimeGenres()
+                .map { $0.data.map { $0.toEntity() } }
+                .map { Mutation.setGenres($0) }
+                .asObservable()
+            
+            return Observable.merge(
+                Observable.just(Mutation.setRecentKeywords(keywords)),
+                genres,
+                topAnime,
+                topCharacters
+            )
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .setRecentKeywords(let keywords):
+            newState.recentKeywords = keywords
+        case .setGenres(let genres):
+            newState.genres = genres
+        case .setTopAnimes(let animes):
+            newState.topAnimes = animes
+        case .setTopCharacters(let characters):
+            newState.topCharacters = characters
+        }
+        return newState
+    }
+}
