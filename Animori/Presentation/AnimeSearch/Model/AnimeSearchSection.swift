4899586eef1c53df2a97b3238898ca0e47d36da0
been <leebin0603@naver.com>
2025-04-05 16:32:59 +0900

    [feat] #17 검색화면 섹션 구현

--- Animori/Presentation/AnimeSearch/Model/AnimeSearchSection.swift
+++ Animori/Presentation/AnimeSearch/Model/AnimeSearchSection.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import RxDataSources
+
+enum AnimeSearchSectionItem {
+    case recentSearch(String)
+    case genreSearch(any AnimeGenreProtocol)
+    case topAnime(any AnimeProtocol)
+    case topCharacter(any TopCharacterProtocol)
+}
+
+struct AnimeSearchSection {
+    let header: String
+    var items: [AnimeSearchSectionItem]
+}
+
+extension AnimeSearchSection: SectionModelType {
+    typealias Item = AnimeSearchSectionItem
+    
+    init(original: AnimeSearchSection, items: [AnimeSearchSectionItem]) {
+        self = original
+        self.items = items
+    }
+}
