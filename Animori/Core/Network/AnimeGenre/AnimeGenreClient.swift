0a5ab955b3a653f5d2b6f6c1a2683496b731e9dd
been <leebin0603@naver.com>
2025-04-05 16:29:19 +0900

    [feat] #17 장르 네트워크 구현

--- Animori/Core/Network/AnimeGenre/AnimeGenreClient.swift
+++ Animori/Core/Network/AnimeGenre/AnimeGenreClient.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import RxSwift
+
+final class AnimeGenreClient {
+    
+    static let shared = AnimeGenreClient()
+    private let provider = NetworkProvider<AnimeGenreEndPoint>()
+    
+    private init() { }
+    
+    func getAnimeGenres() -> Single<AnimeGenreResponseDTO> {
+        return provider.request(.genres)
+            .catch { error in
+                return Single.just(AnimeGenreResponseDTO.empty)
+            }
+    }
+    
+}
--- Animori/Core/Network/AnimeGenre/AnimeGenreEndPoint.swift
+++ Animori/Core/Network/AnimeGenre/AnimeGenreEndPoint.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+enum AnimeGenreEndPoint: EndPoint {
+    
+    case genres // getAnimeGenres
+    
+    var baseURL: String? { return Bundle.main.baseURL }
+    var path: String {
+        switch self {
+        case .genres: return "/genres/anime"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .genres: return .get
+        }
+    }
+    
+    var decoder: JSONDecoder { return JSONDecoder() }
+    
+    var encoder: JSONEncoder { return JSONEncoder() }
+    
+    var parameters: Encodable {
+        switch self {
+        case .genres: return EmptyParameters()
+        }
+    }
+    
+    func error(_ statusCode: Int?, data: Data) -> any Error {
+        return URLError(.unknown) // 임시
+    }
+    
+}
