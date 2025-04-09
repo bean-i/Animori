0e29caccc2aba50e71cab06dee7b02e8d8f4902c
been <leebin0603@naver.com>
2025-04-10 03:35:28 +0900

    [feat] #27 캐릭터 상세화면 로직 구현

--- Animori/Presentation/AnimeCharacterDetail/AnimeCharacterDetailViewController.swift
+++ Animori/Presentation/AnimeCharacterDetail/AnimeCharacterDetailViewController.swift
@@ -5,4 +5,78 @@
 //  Created by 이빈 on 4/9/25.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import ReactorKit
+
+final class AnimeCharacterDetailViewController: BaseViewController<AnimeCharacterDetailView> {
+    
+    var disposeBag = DisposeBag()
+    
+    init(reactor: AnimeCharacterDetailViewModel) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+}
+
+extension AnimeCharacterDetailViewController: View {
+    
+    func bind(reactor: AnimeCharacterDetailViewModel) {
+        reactor.action.onNext(.loadInfo)
+        
+        // 로딩 인디케이터
+        reactor.pulse(\.$isLoading)
+            .observe(on: MainScheduler.instance)
+            .bind(with: self) { owner, isLoading in
+                if isLoading {
+                    Loading.shared.showLoading(in: owner.mainView)
+                } else {
+                    Loading.shared.hideLoading(from: owner.mainView)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        // 캐릭터 사진
+        reactor.state
+            .map { $0.characterPictures }
+            .observe(on: MainScheduler.instance)
+            .bind(with: self) { owner, images in
+                owner.mainView.characterImages = images
+                owner.mainView.imagePagerView.reloadData()
+            }
+            .disposed(by: disposeBag)
+        
+        // 캐릭터 정보
+        reactor.state
+            .map { $0.characterInfo }
+            .observe(on: MainScheduler.instance)
+            .bind(with: self) { owner, info in
+                owner.navigationItem.title = info.name
+                owner.mainView.configureData(data: info)
+            }
+            .disposed(by: disposeBag)
+        
+        
+        // 캐릭터 성우 정보
+        reactor.state
+            .map { $0.characterVoiceActors }
+            .observe(on: MainScheduler.instance)
+            .bind(to: mainView.voiceActorCollectionView.rx.items(cellIdentifier: CharacterCell.identifier, cellType: CharacterCell.self)) { (row, element, cell) in
+                cell.configureVoiceActor(element)
+            }
+            .disposed(by: disposeBag)
+        
+        // 에러
+        reactor.pulse(\.$error)
+            .compactMap { $0 }
+            .observe(on: MainScheduler.instance)
+            .bind(with: self) { owner, _ in
+                let alert = DIContainer.shared.makeAlert(retryAction: {
+                    owner.reactor?.action.onNext(.loadInfo)
+                })
+                owner.present(alert, animated: true)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+}
--- Animori/Presentation/AnimeCharacterDetail/AnimeCharacterDetailViewModel.swift
+++ Animori/Presentation/AnimeCharacterDetail/AnimeCharacterDetailViewModel.swift
@@ -6,3 +6,86 @@
 //
 
 import Foundation
+import ReactorKit
+
+final class AnimeCharacterDetailViewModel: Reactor {
+    
+    enum Action {
+        case loadInfo
+    }
+    
+    enum Mutation {
+        case setLoading(Bool)
+        case setCharacterPictures([AnimeCharacterPicturesProtocol])
+        case setCharacterInfo(AnimeCharacterInfoProtocol)
+        case setCharacterVoiceActors([AnimeCharacterVoiceActorsProtocol])
+        case setError(Error)
+    }
+    
+    struct State {
+        @Pulse var isLoading: Bool = false
+        var characterPictures: [AnimeCharacterPicturesProtocol] = []
+        var characterInfo: AnimeCharacterInfoProtocol = AnimeCharacterInfoDTO.empty.toEntity()
+        var characterVoiceActors: [AnimeCharacterVoiceActorsProtocol] = []
+        @Pulse var error: Error? = nil
+    }
+    
+    private let characterID: Int
+    let initialState: State
+    
+    init(initialState: State, characterID: Int) {
+        self.initialState = initialState
+        self.characterID = characterID
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .loadInfo:
+            let startLoading = Observable.just(Mutation.setLoading(true))
+            
+            let characterPictures = AnimeCharacterClient.shared.getCharacterPictures(id: characterID)
+                .map { Mutation.setCharacterPictures($0.data.map { $0.toEntity() }) }
+                .catch { error in
+                    Single.just(Mutation.setError(error))
+                }
+                .asObservable()
+            
+            let characterInfo = AnimeCharacterClient.shared.getCharacterById(id: characterID)
+                .map { Mutation.setCharacterInfo($0.data.toEntity()) }
+                .catch { error in
+                    Single.just(Mutation.setError(error))
+                }
+                .asObservable()
+            
+            let characterVoiceActors = AnimeCharacterClient.shared.getCharacterVoiceActors(id: characterID)
+                .map { Mutation.setCharacterVoiceActors($0.data.map { $0.toEntity() }) }
+                .catch { error in
+                    Single.just(Mutation.setError(error))
+                }
+                .asObservable()
+            
+            let networkRequests = Observable.merge(characterPictures, characterInfo, characterVoiceActors)
+            let finishLoading = Observable.just(Mutation.setLoading(false))
+            
+            return Observable.concat(
+                startLoading,
+                networkRequests,
+                finishLoading
+            )
+            
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .setLoading(let bool): newState.isLoading = bool
+        case .setCharacterPictures(let pictures): newState.characterPictures = pictures
+        case .setCharacterInfo(let info): newState.characterInfo = info
+        case .setCharacterVoiceActors(let actors): newState.characterVoiceActors = actors
+        case .setError(let error): newState.error = error
+        }
+        return newState
+    }
+    
+}
