0df45348f34ea0262796cd42577b0db6504c13b2
been <leebin0603@naver.com>
2025-04-10 21:37:29 +0900

    [feat] #27 캐릭터 리스트 화면 UI 구현

--- Animori/Presentation/AnimeCharacterList/AnimeCharacterListView.swift
+++ Animori/Presentation/AnimeCharacterList/AnimeCharacterListView.swift
@@ -5,4 +5,52 @@
 //  Created by 이빈 on 4/10/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class AnimeCharacterListView: BaseView {
+    
+    let characterListCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
+    override func configureHierarchy() {
+        addSubview(characterListCollectionView)
+    }
+    
+    override func configureLayout() {
+        characterListCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(10)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
+    }
+    
+    override func configureView() {
+        backgroundColor = .am(.base(.black))
+
+        characterListCollectionView.backgroundColor = .am(.base(.black))
+        characterListCollectionView.showsVerticalScrollIndicator = false
+        characterListCollectionView.showsHorizontalScrollIndicator = false
+        characterListCollectionView.register(CharacterInfoCell.self, forCellWithReuseIdentifier: CharacterInfoCell.identifier)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        characterListCollectionView.collectionViewLayout = configureCharacterListCollectionViewLayout()
+        layoutIfNeeded()
+    }
+    
+    private func configureCharacterListCollectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let inset: CGFloat = 20
+        let spacing: CGFloat = 8
+        let totalWidth = frame.width
+        let itemWidth = (totalWidth - (2 * inset) - (2 * spacing)) / 3
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = spacing
+        layout.minimumLineSpacing = inset
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth * 1.6)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: inset, bottom: inset, right: inset)
+        return layout
+    }
+    
+}
+
