2f50f9f0379089a396775061ac2cd3b1bf4cda07
been <leebin0603@naver.com>
2025-04-12 00:25:27 +0900

    [chore] #31 Floaty 패키지 추가

---
+++ Animori/Core/Realm/AnimeSaveStatus/AnimeSaveStatusRepository.swift
@@ -0,0 +1,8 @@
+//
+//  AnimeSaveStatusRepository.swift
+//  Animori
+//
+//  Created by 이빈 on 4/11/25.
+//
+
+import Foundation
---
+++ Animori/Core/Realm/AnimeSaveStatus/AnimeSaveStatusTable.swift
@@ -0,0 +1,8 @@
+//
+//  AnimeSaveStatusTable.swift
+//  Animori
+//
+//  Created by 이빈 on 4/11/25.
+//
+
+import Foundation
