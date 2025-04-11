24a3dbb0f6a1ea8ce46961d1dc61ed087ca89dce
been <leebin0603@naver.com>
2025-04-12 02:56:14 +0900

    [feat] #31 저장화면 구현

--- Animori/Presentation/Storage/StorageSection.swift
+++
@@ -1,8 +0,0 @@
-//
-//  StorageSection.swift
-//  Animori
-//
-//  Created by 이빈 on 4/12/25.
-//
-
-import Foundation
--- Animori/Presentation/Storage/StorageView.swift
+++ Animori/Presentation/Storage/StorageView.swift
@@ -5,4 +5,72 @@
 //  Created by 이빈 on 4/12/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class StorageView: BaseView {
+    
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
+    override func configureHierarchy() {
+        addSubview(collectionView)
+    }
+    
+    override func configureLayout() {
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    override func configureView() {
+        backgroundColor = .am(.base(.black))
+        collectionView.backgroundColor = .am(.base(.black))
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        
+        collectionView.collectionViewLayout = configureCollectionViewLayout()
+        
+        collectionView.register(RecommendCollectionViewCell.self, forCellWithReuseIdentifier: RecommendCollectionViewCell.identifier)
+        collectionView.register(SectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SectionHeaderView.identifier)
+    }
+    
+    private func configureCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { [weak self] sectionIndex, _ in
+            self?.configureRecommendLayout()
+        }
+    }
+    
+    private func configureRecommendLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .absolute(220),
+            heightDimension: .absolute(130)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .absolute(220),
+            heightDimension: .absolute(130)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        group.interItemSpacing = .fixed(10)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.interGroupSpacing = 10
+        
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(22)
+        )
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+        section.boundarySupplementaryItems = [header]
+        
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 30, trailing: 20)
+        return section
+    }
+    
+}
